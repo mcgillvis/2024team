dc0baa5cc4f29641768979667d49e97af86d4d5a
syzhao53 <syzhao@sas.upenn.edu>
2023-10-02 23:56:10 -0500

    links + desktop view

--- site/src/App.js
+++ site/src/App.js
@@ -3,7 +3,6 @@ import React, { useEffect } from 'react';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Name from './components/Name';
 import Eva from './assets/eva-headshot.jpeg';
 import Wing from './assets/wing-headshot.jpeg';
 import Laura from './assets/laura-headshot.jpeg';
@@ -32,8 +31,12 @@ function App() {
                 <div className="profile-name">Somaya Amiri</div>
                 <div className="profile-subheader">BCL/JD Candidate</div>
                 <div className="contact-wrap">
-                  <img className="email-icon" src={email} alt="email icon" />
-                  <img className="linkedin-icon" src={linkedin} alt="email icon" />
+                  <a href="mailto:somaya.amiri@mail.mcgill.ca">
+                    <img className="email-icon" src={email} alt="email icon" />
+                  </a>
+                  <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/somaya-amiri1/">
+                    <img className="linkedin-icon" src={linkedin} alt="email icon" />
+                  </a>
                 </div>
               </div>
               <div className="profile-flex">
@@ -41,8 +44,12 @@ function App() {
                 <div className="profile-name">Laura Andrade</div>
                 <div className="profile-subheader">BCL/JD Candidate</div>
                 <div className="contact-wrap">
-                  <img className="email-icon" src={email} alt="email icon" />
-                  <img className="linkedin-icon" src={linkedin} alt="email icon" />
+                  <a href="mailto:laura.andrade2@mail.mcgill.ca">
+                    <img className="email-icon" src={email} alt="email icon" />
+                  </a>
+                  <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/lauraandrademontreal/">
+                  <img className="linkedin-icon" src={linkedin} alt="linkedin logo icon" />
+                  </a>
                 </div>
               </div>
             </div>
@@ -52,17 +59,25 @@ function App() {
                 <div className="profile-name">Wing Wong</div>
                 <div className="profile-subheader">BCL/JD Candidate</div>
                 <div className="contact-wrap">
-                  <img className="email-icon" src={email} alt="email icon" />
-                  <img className="linkedin-icon" src={linkedin} alt="email icon" />
+                  <a href="mailto:wing.wong@mail.mcgill.ca">
+                    <img className="email-icon" src={email} alt="email icon" />
+                  </a>
+                  <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/wing-w-061851128/">
+                  <img className="linkedin-icon" src={linkedin} alt="linkedin logo icon" />
+                  </a>
                 </div>
               </div>
-              <div className="profile-flex">
+              <div className="profile-flex-last">
                 <img className="headshot" src={Eva} alt="Eva Wu headshot"/>
                 <div className="profile-name">Eva Wu</div>
                 <div className="profile-subheader">BCL/JD Candidate</div>
                 <div className="contact-wrap">
-                  <img className="email-icon" src={email} alt="email icon" />
-                  <img className="linkedin-icon" src={linkedin} alt="email icon" />
+                  <a href="mailto:eva.wu@mail.mcgill.ca">
+                    <img className="email-icon" src={email} alt="email icon" />
+                  </a>
+                  <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/eva-wu-a127b812b/">
+                    <img className="linkedin-icon" src={linkedin} alt="linkedin logo icon" />
+                  </a>
                 </div>
               </div>
             </div>
@@ -76,18 +91,40 @@ function App() {
                 <img className="headshot" src={Eric} alt="Eric Bédard headshot"/>
                 <div className="profile-name">Eric Bédard</div>
                 <div className="profile-subheader">Partner, Woods LLP</div>
+                <div className="contact-wrap">
+                  <a href="mailto:ebedard@woods.qc.ca">
+                    <img className="email-icon" src={email} alt="email icon" />
+                  </a>
+                  <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/eric-bedard-woods/">
+                  <img className="linkedin-icon" src={linkedin} alt="linkedin logo icon" />
+                  </a>
+                </div>
               </div>
-              <div className="profile-flex">
+              <div className="profile-flex-last">
                 <img className="headshot" src={Dina} alt="Dina Prokic headshot"/>
                 <div className="profile-name">Dina Prokic</div>
                 <div className="profile-subheader">Associate, Woods LLP</div>
+                <div className="contact-wrap">
+                  <a href="mailto:dprokic@woods.qc.ca">
+                    <img className="email-icon" src={email} alt="email icon" />
+                  </a>
+                  <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/dina-p-35965560/">
+                  <img className="linkedin-icon" src={linkedin} alt="linkedin logo icon" />
+                  </a>
+                </div>
               </div>
             </div>
           </div>
         </div>
         <div className="section-wrap">
           <div className="section-header">Thank you to our sponsors</div>
-          <div className="spacer">Coming soon! Contact our team to sponsor us.</div>
+          <div className="sponsors-flex">
+            <div className="text-spacer-r">Coming soon!</div>
+            <a id="contact-team-link" href="mailto:somaya.amiri@mail.mcgill.ca,laura.andrade2@mail.mcgill.ca,eva.wu@mail.mcgill.ca,wing.wong@mail.mcgill.ca">
+              Contact our team
+            </a>
+            <div className="text-spacer-l">to sponsor us.</div>
+          </div>
         </div>
       </div>
       <Footer />
